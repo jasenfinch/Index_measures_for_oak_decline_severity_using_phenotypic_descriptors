5c87a72a436064072204e339ab0873037ea20d50
jasenfinch <jsf9@aber.ac.uk>
2019-07-12 13:31:36 +0100

    added descriptor adjustment figure

--- code/data_prep.R
+++ code/data_prep.R
@@ -28,3 +28,18 @@ phenoData <- {
 }
 
 DIs <- calcDIs()
+
+correctedPhenoData <- siteCorrection(phenoData) %>%
+  mutate(`Crown condition (%)` = crownCondition(`Missing crown (%)`,
+                                                `Crown transparency (%)`),
+         `Crown volume (m^3)` = crownVolume(`Crown radius (m)`,
+                                            `Total height (m)`,
+                                            `Lower crown height (m)`,
+                                            `Crown condition (%)`),
+         `Bleed prevalence (%)` = bleedPrevalence(`Active bleed size (cm)`,
+                                                  `Active bleeds`,
+                                                  `Black staining size (cm)`,
+                                                  `Black staining`,
+                                                  `Diameter at breast height (cm)`),
+         `Agrilus exit hole density (m^-2)` = agrilusExitHoleDensity(`Agrilus exit holes`,`Diameter at breast height (cm)`)
+  )
