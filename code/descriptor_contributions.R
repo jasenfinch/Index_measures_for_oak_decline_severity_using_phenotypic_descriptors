67c0ff426cb79da2a8bdabf55156460e07c0bc2e
jasenfinch <jsf9@aber.ac.uk>
2019-08-28 21:15:08 +0100

    added PDI response surfaces

--- code/descriptor_contributions.R
+++ code/descriptor_contributions.R
@@ -1,39 +1,64 @@
 
 descriptor_contributions <- list(
-  a = PDIrf %>%
-  {
-    imp <- importance(.)
-    feat <- rownames(imp)
-    imp <- imp[,1]
-    tibble(Feature = feat,`Gini Importance` = imp)
-  } %>%
-  arrange(`Gini Importance`) %>%
-  mutate(Feature = factor(Feature,levels = Feature), `Relative Importance` = `Gini Importance` / max(`Gini Importance`)) %>%
-  {ggplot(.,aes(x = Feature,y = `Relative Importance`)) +
-      geom_point(colour = ptol_pal()(1)) +
+  a = {
+    dat <- PDIrf %>%
+      {
+        imp <- importance(.)
+        feat <- rownames(imp)
+        imp <- imp[,1]
+        tibble(Feature = feat,`Gini Importance` = imp)
+      } %>%
+      arrange(`Gini Importance`) %>%
+      mutate(Feature = factor(Feature,levels = Feature), `Relative Importance` = `Gini Importance` / max(`Gini Importance`)) 
+    
+    descriptorLabels <- dat$Feature %>%
+      as.character() %>%
+      {c(.[1:5],
+         expression(Agrillus~exit~hole~density ( m^-2 ) ),
+         .[7:33],
+         expression(Crown~volume ( m^3 ) )
+      )}
+    
+    ggplot(dat,aes(x = Feature,y = `Relative Importance`)) +
+      geom_point(fill = ptol_pal()(1),shape = 21) +
       coord_flip() +
       theme_bw() +
-      theme(plot.title = element_text(face = 'bold',size = 9),
-            axis.title = element_text(face = 'bold')) +
-      labs(title = 'Descriptor importance\nfor PDI',
-           y = 'Relative importance',
-           x = 'Descriptor')},
-  b = DAIrf %>%
+      theme(plot.title = element_text(face = 'bold'),
+            axis.title = element_text(face = 'bold',size = 10)) +
+      labs(title = 'a)',
+           y = 'Relative Gini\nimportance',
+           x = NULL) +
+      scale_x_discrete(labels = descriptorLabels)
+  },
+  b = {
+    dat <- DAIrf %>%
     {
       imp <- importance(.)
       feat <- rownames(imp)
       imp <- imp[,1]
       tibble(Feature = feat,`Gini Importance` = imp)
     } %>%
-  arrange(`Gini Importance`) %>%
-  mutate(Feature = factor(Feature,levels = Feature), `Relative Importance` = `Gini Importance` / max(`Gini Importance`)) %>%
-  {ggplot(.,aes(x = Feature,y = `Relative Importance`)) +
-      geom_point(colour = ptol_pal()(1)) +
-      coord_flip() +
-      theme_bw() +
-      theme(plot.title = element_text(face = 'bold',size = 9),
-            axis.title = element_text(face = 'bold')) +
-      labs(title = 'Descriptor importance\nfor DAI',
-           y = 'Relative importance',
-           x = 'Descriptor')}
+    arrange(`Gini Importance`) %>%
+    mutate(Feature = factor(Feature,levels = Feature), `Relative Importance` = `Gini Importance` / max(`Gini Importance`))
+    
+    descriptorLabels <- dat$Feature %>%
+      as.character() %>%
+      {c(.[1:10],
+         expression(Agrillus~exit~hole~density ( m^-2 ) ),
+         .[12:30],
+         expression(Crown~volume ( m^3 ) ),
+         .[32:34]
+      )}
+    
+    ggplot(dat,aes(x = Feature,y = `Relative Importance`)) +
+        geom_point(fill = ptol_pal()(1),shape = 21) +
+        coord_flip() +
+        theme_bw() +
+        theme(plot.title = element_text(face = 'bold'),
+              axis.title = element_text(face = 'bold',size = 10)) +
+        labs(title = 'b)',
+             y = 'Relative Gini\nimportance',
+             x = NULL) +
+      scale_x_discrete(labels = descriptorLabels)
+  }
 )
--- code/library_load.R
+++ code/library_load.R
@@ -1,1 +1,1 @@
-pacman::p_load(maps,mapdata,pdi,ggthemes,randomForest,patchwork,gridExtra,ggrepel,tidyverse)
+pacman::p_load(maps,mapdata,pdi,ggthemes,randomForest,patchwork,gridExtra,ggrepel,metR,tidyverse,ggpubr)
