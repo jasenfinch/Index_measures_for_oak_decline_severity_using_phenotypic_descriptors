a74161a2d6168668625835363b29a3c32486c6e9
jasenfinch <jsf9@aber.ac.uk>
2019-11-11 13:29:29 +0000

    warnings and messages now not included. Sorted library loading errors

--- code/library_load.R
+++ code/library_load.R
@@ -1,1 +1,3 @@
-pacman::p_load(maps,mapdata,pdi,ggthemes,randomForest,patchwork,gridExtra,ggrepel,metR,tidyverse,ggpubr)
+pacman::p_load(maps,mapdata,pdi,ggthemes,randomForest,
+               thomasp85/patchwork,gridExtra,ggrepel,metR,tidyverse,
+               ggpubr,wordcountaddin,spelling,ropenscilabs/gramr)
