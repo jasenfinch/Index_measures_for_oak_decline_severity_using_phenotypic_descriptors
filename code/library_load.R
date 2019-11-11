bbacf100ea9852254549d060ac6bdcfc7fe847e5
jasenfinch <jsf9@aber.ac.uk>
2019-11-11 13:48:04 +0000

    figures included within manuscript

--- code/library_load.R
+++ code/library_load.R
@@ -1,3 +1,3 @@
 pacman::p_load(maps,mapdata,pdi,ggthemes,randomForest,
-               thomasp85/patchwork,gridExtra,ggrepel,metR,tidyverse,
-               ggpubr,wordcountaddin,spelling,ropenscilabs/gramr)
+               patchwork,gridExtra,ggrepel,metR,tidyverse,
+               ggpubr,wordcountaddin,spelling,gramr)
