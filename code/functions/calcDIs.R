5b909b7ff87d81544992724f1167955a053b5dfc
jasenfinch <jsf9@aber.ac.uk>
2019-11-12 14:33:41 +0000

    fixed DAI inversion

--- code/functions/calcDIs.R
+++ code/functions/calcDIs.R
@@ -4,8 +4,7 @@ calcDIs <- function(rfModels,phenoData){
     mds() %>%
     rename(PDI = `Dimension 1`,DAI = `Dimension 2`) %>%
     mutate(PDI = minMaxScale(PDI),
-           DAI = (2 * minMaxScale(DAI) - 1) * -1) %>%
-    # mutate(DAI = 1 - DAI,PDI = 1 - PDI) %>%
+           DAI = (2 * minMaxScale(DAI) - 1)) %>%
     bind_cols(phenoData %>%
                 select(Location,`Tree No`,Status,ChosenGroup))
 }
