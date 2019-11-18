434343f9440b2c8262065f9028ce58b8b40f91da
jasenfinch <jsf9@aber.ac.uk>
2019-11-18 13:23:45 +0000

    removed code chunks from supplementary

--- code/functions/declineIndexesPlot.R
+++ code/functions/declineIndexesPlot.R
@@ -6,10 +6,8 @@ declineIndexesPlot <- function(decline_indexes){
     geom_point(aes(fill = ChosenGroup),shape = 21,size = 3) +
     scale_fill_manual(values = ptol_pal()(4)[c(4,1,2,3)]) +
     theme_bw() +
-    #facet_wrap(~Location) +
     theme(legend.position = 'bottom',
           axis.title = element_text(face = 'bold'),
           legend.title = element_text(face = 'bold')) +
-    labs(fill = 'Assigned Decline Status') +
-    coord_fixed()
+    labs(fill = 'Assigned Decline Status')
 }
