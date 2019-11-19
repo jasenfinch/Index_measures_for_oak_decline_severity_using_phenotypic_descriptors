526df51bc7e9edd39a3a91eb704a1ea4080f9f3f
jasenfinch <jsf9@aber.ac.uk>
2019-11-19 16:03:06 +0000

    inverted DAI

--- code/functions/calcDIs.R
+++ code/functions/calcDIs.R
@@ -3,8 +3,12 @@ calcDIs <- function(rfModels,phenoData){
   rfModels %>%
     mds() %>%
     rename(PDI = `Dimension 1`,DAI = `Dimension 2`) %>%
-    mutate(PDI = minMaxScale(PDI),
-           DAI = (2 * minMaxScale(DAI) - 1)) %>%
+    mutate(PDI = PDI %>%
+             minMaxScale(),
+           DAI = DAI %>%
+             {. * -1} %>%
+             minMaxScale() %>%
+             {2 * . - 1}) %>%
     bind_cols(phenoData %>%
                 select(Location,`Tree No`,Status,ChosenGroup))
 }
