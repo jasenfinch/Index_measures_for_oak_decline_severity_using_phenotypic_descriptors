197172c79cd74b7184e8abcaaea061e131534344
jasenfinch <jsf9@aber.ac.uk>
2019-11-25 10:53:29 +0000

    added to descriptor table

--- code/functions/descriptorInfo.R
+++ code/functions/descriptorInfo.R
@@ -4,47 +4,52 @@
 
 descriptorInfo <- function(analysisTable){
   pt <- analysisTable %>%
-    {tibble(Descriptor = names(.),`Data Type` = map_chr(.,class),Description = NA,`Categories / units` = NA,Reference = NA)} %>%
+    {tibble(Descriptor = names(.),`Data Type` = map_chr(.,class))} %>%
     filter(!(Descriptor %in% c('Crown condition (%)','Crown volume (m^3)','Bleed prevalence (%)','Agrilus exit hole density (m^-2)')))
   
-  type <- tibble(`Descriptor Type` = c(rep('tree size',5),
-                                       rep('crown',13),
-                                       rep('trunk',13)),
-                 Descriptor = c('Diameter at breast height (cm)',
-                                'Crown radius (m)',
-                                'Total height (m)',
-                                'Lower crown height (m)',
-                                'Timber height (m)',
-                                'Missing crown (%)',
-                                'Crown transparency (%)',
-                                'Crown contact (%)',
-                                'Crown mildew',
-                                'Branch epicormics',
-                                'Crown fruiting bodies',
-                                'Dieback location',
-                                'Dieback type','Insect defoliation',
-                                'Insect defoliation type',
-                                'Pruning / branch loss',
-                                'Canopy closure',
-                                'Social class',
-                                'Agrilus exit holes',
-                                'Stem epicormics',
-                                'Dead stem tissue',
-                                'Ground level fruiting bodies',
-                                'Stem fruiting bodies',
-                                'Oval shaped exit holes',
-                                'Small circular shaped exit holes',
-                                'Active bleeds',
-                                'Active bleed size (cm)',
-                                'Black staining',
-                                'Black staining size (cm)',
-                                'Calloused wound',
-                                'Calloused wound size (cm)')
-  )
+  type <- tribble(
+    ~Descriptor,~Description,~`Categories / units`,~Reference,
+    'Diameter at breast height (mm)','','mm','',
+    'Crown radius (m)','','m','',
+    'Total height (m)','','m','',
+    'Lower crown height (m)','','m','',
+    'Timber height (m)','','m','',
+    'Missing crown (%)','','%','',
+    'Crown transparency (%)','','%','',
+    'Crown contact (%)','','%','',
+    'Crown mildew','','','',
+    'Branch epicormics','','','',
+    'Crown fruiting bodies','','','',
+    'Dieback location','','','',
+    'Dieback type','','','',
+    'Insect defoliation','','','',
+    'Insect defoliation type','','','',
+    'Pruning / branch loss','','','',
+    'Canopy closure','','','',
+    'Social class','','','',
+    'Agrilus exit holes','Number of *Agrilus* exit holes.','frequency','',
+    'Stem epicormics','','','',
+    'Dead stem tissue','','','',
+    'Ground level fruiting bodies','','','',
+    'Stem fruiting bodies','','','',
+    'Oval shaped exit holes','','','',
+    'Small circular shaped exit holes','','','',
+    'Active bleeds','Number of active stem bleeds.','frequency','',
+    'Active bleed size (mm)','','mm','',
+    'Black staining','','','',
+    'Black staining size (mm)','','mm','',
+    'Calloused wound','','','',
+    'Calloused wound size (mm)','','mm',''
+  ) %>%
+    mutate(`Descriptor Type` = c(rep('tree size',5),
+                                 rep('crown',13),
+                                 rep('stem',13)))
   
   pt <- pt %>%
     left_join(type, by = "Descriptor") %>%
-    mutate(Descriptor = str_to_lower(Descriptor)) %>%
+    mutate(Descriptor = str_to_lower(Descriptor) %>%
+             str_remove_all('\\((.*?)\\)') %>%
+             str_replace_all('Agrilus','*Agrilus*')) %>%
     select(`Descriptor Type`,everything()) %>%
     arrange(`Descriptor Type`,Descriptor)
   
