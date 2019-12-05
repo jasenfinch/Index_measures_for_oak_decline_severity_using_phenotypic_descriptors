8e5daa0df937d96d6653525f1c25cd35bee75804
jasenfinch <jsf9@aber.ac.uk>
2019-12-05 12:40:50 +0000

    added to descriptor table

--- R/functions/descriptorInfo.R
+++ R/functions/descriptorInfo.R
@@ -14,32 +14,32 @@ descriptorInfo <- function(analysisTable){
     'Total height (m)','Total tree height.','m','',
     'Lower crown height (m)','Lowest live crown height to within 0.1m.','m','',
     'Timber height (m)','','m','',
-    'Missing crown (%)','','%','',
-    'Crown transparency (%)','','%','',
+    'Missing crown (%)','Percentage of missing crown.','%','',
+    'Crown transparency (%)','Absolute crown transparency in 5% classes.','%','',
     'Crown contact (%)','','%','',
-    'Crown mildew','','','',
-    'Branch epicormics','','','',
-    'Crown fruiting bodies','','','',
-    'Dieback location','','','',
-    'Dieback type','','','',
-    'Insect defoliation','','','',
-    'Insect defoliation type','','','',
+    'Crown mildew','Extent of crown mildew.','0 = none; 1 = rare, only a few leaves affected; 2 = infrequent, less than 5% of leaves affected; 3 = common, manly leaves affected; 4 = abundant, most leaves affected','',
+    'Branch epicormics','Epicormic growth on branches.','0 = none; 1 = rare; 2 = scattered, one or two main branches with them; 3 = common, several main branches with them; 4 = abundant, majority of main branches with them','',
+    'Crown fruiting bodies','Presence of crown fungal fruiting bodies. Any species.','P = present; A = absent','',
+    'Dieback location','Main crown dieback location.','0 = none; 1 = top of the tree only; 2 = middle parts of the crown; 3 = top and middle of the crown; 4 = bottom and middle of the crown; 5 = branches at the base of the crown; 6 = throughout the crown','',
+    'Dieback type','Type of crown dieback.','0 = none; 1 = leaf loss only, with lateral branches partly or completely bare; 2 = dieback restricted to relatively thin branches; 3 = several large branches involved; 4 = main stem involved in the upper part of the crown; 5 = restricted to twigs','',
+    'Insect defoliation','Extent of leaf damage caused by insect herbivory.','0 = none; 1 = rare, only a few leaves affected; 2 = infrequent leass than 5% of leaves affected; 3 = common, many leaves affected; 4 = abundant, most leaves affected','',
+    'Insect defoliation type','Type of insect defoliation.','Pin = pinhole damage; Chin = chewing damage; Both = both pinhole and chewing damage','',
     'Pruning / branch loss','','P = present; A = absent','',
-    'Canopy closure','','','',
-    'Social class','','1 = dominant; 2 = codominant; 3 = subdominant; 4 = suppressed','',
-    'Agrilus exit holes','Number of *Agrilus* exit holes.','frequency','',
-    'Stem epicormics','','','',
+    'Canopy closure','Adjacent tree canopies in contact with canopy.','Y = yes; N = no','',
+    'Social class','Social class','1 = dominant; 2 = codominant; 3 = subdominant; 4 = suppressed','',
+    'Agrilus exit holes','Number of *Agrilus* exit holes from the tree base to a height of 2m.','frequency','',
+    'Stem epicormics','Epicormic growth on the main stem.','0 = none; 1 = rare; 2 = scattered (11-50); 3 = common (51-100); 4 = abundant (stem totally obscured by epicormics','',
     'Dead stem tissue','','','',
-    'Ground level fruiting bodies','','','',
-    'Stem fruiting bodies','','','',
-    'Oval shaped exit holes','','','',
-    'Small circular shaped exit holes','','','',
-    'Active bleeds','Number of active stem bleeds.','frequency','',
-    'Active bleed size (mm)','','mm','',
-    'Black staining','','','',
-    'Black staining size (mm)','','mm','',
-    'Calloused wound','','','',
-    'Calloused wound size (mm)','','mm',''
+    'Ground level fruiting bodies','Presence of ground level fungal fruiting bodies. Any species.','P = present; A = absent','',
+    'Stem fruiting bodies','Presence of fungal fruiting bodies on the main stem.','P = present; A = absent','',
+    'Oval shaped exit holes','Presence of oval shaped exit holes.','P = present; A = absent','',
+    'Small circular shaped exit holes','','P = present; A = absent','',
+    'Active bleeds','Number of active stem bleeds in 3m section from the base of the tree. Liquid running.','frequency','',
+    'Active bleed size (mm)','Average active bleed size in the 3m section from the base of the tree.','mm','',
+    'Black staining','Number of black stains in the 3m section from the base of the tree. Inactive bleeds.','fequency','',
+    'Black staining size (mm)','Average black stain size in the 3m section from the base of the tree.','mm','',
+    'Calloused wound','Number of callused wounds in the 3m section from the base of the tree. Scars.','frequency','',
+    'Calloused wound size (mm)','Average callouse wound size in the 3m section from the base of the tree.','mm',''
   ) %>%
     mutate(`Descriptor Type` = c(rep('tree size',5),
                                  rep('crown',13),
@@ -53,7 +53,7 @@ descriptorInfo <- function(analysisTable){
     select(`Descriptor Type`,everything()) %>%
     arrange(`Descriptor Type`,Descriptor)
   
-  pt$`Data Type`[pt$`Data Type` == 'numeric'] <- 'continous'
+  pt$`Data Type`[pt$`Data Type` == 'numeric'] <- 'continuous'
   pt$`Data Type`[pt$`Data Type` == 'factor'] <- 'categorical'
   
   return(pt)
