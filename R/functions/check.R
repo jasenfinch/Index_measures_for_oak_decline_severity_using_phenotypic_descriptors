625c70ffa91254b15016f4526b4a96e66ab60571
jasenfinch <jsf9@aber.ac.uk>
2019-12-10 17:49:38 +0000

    manuscript check types can be selected

--- R/functions/check.R
+++ R/functions/check.R
@@ -1,23 +1,30 @@
 
-manuscript_check <- function(file = 'manuscript.Rmd'){
+manuscript_check <- function(file = 'manuscript.Rmd', checks = c('word count','spelling','grammar')){
   
   options(knitr.table.format = 'markdown')
   
   cat('######### Manuscript Checks #########\n')
-  cat('\nWord count')
   
-  suppressMessages({
-    wordcountaddin::text_stats(file) %>%
-      print()
-  })
+  if ('word count' %in% checks) {
+    cat('\nWord count')
+    
+    suppressMessages({
+      wordcountaddin::text_stats(file) %>%
+        print()
+    }) 
+  }
   
+  if ('spelling' %in% checks) {
   cat('\nSpelling\n\n')
   
   spelling::spell_check_files(file,lang = 'en_GB') %>%
     print()
+  }
   
-  cat('\nStyle')
+  if ('grammar' %in% checks) {
+  cat('\nGrammar')
   
   gramr::write_good_file(file) %>%
     print()
+  }
 }
