ec8363dd988f7be49662bb955de591209a703932
jasenfinch <jsf9@aber.ac.uk>
2020-06-01 13:11:58 +0100

    small aesthetic tweaks for supplementary descriptor scatter plots

--- R/functions/descriptorScatterPlots.R
+++ R/functions/descriptorScatterPlots.R
@@ -17,7 +17,8 @@ descriptorScatterPlots <- function(site_corrected_analysis_suitable_data,decline
            x = 'Descriptor value') +
       theme(plot.title = element_text(face = 'bold'),
             axis.title = element_text(face = 'bold'),
-            strip.text = element_markdown()),
+            strip.text = element_markdown(),
+            panel.grid = element_blank()),
     
     PDI_factor =  site_corrected_analysis_suitable_data %>%
       select_if(is.factor) %>%
@@ -25,8 +26,7 @@ descriptorScatterPlots <- function(site_corrected_analysis_suitable_data,decline
       bind_cols(select(decline_indexes,PDI)) %>%
       gather(Descriptor,Value,-ID,-PDI) %>%
       mutate(Descriptor = factor(Descriptor) %>%
-               fct_recode(`Agrilus exit hole density (m<sup>-2</sup>)` = 'Agrilus exit hole density (m^-2)',
-                          `Crown volume (m<sup>3</sup>)` = 'Crown volume (m^3)')) %>%
+               fct_recode(`Small circular shaped<br>exit holes` = "Small circular shaped exit holes")) %>%
       ggplot(aes(x = Value,y = PDI)) +
       geom_point(shape = 21,fill = ptol_pal()(1)) +
       theme_bw() +
@@ -35,7 +35,8 @@ descriptorScatterPlots <- function(site_corrected_analysis_suitable_data,decline
            x = 'Descriptor value') +
       theme(plot.title = element_text(face = 'bold'),
             axis.title = element_text(face = 'bold'),
-            strip.text = element_markdown()),
+            strip.text = element_markdown(),
+            panel.grid = element_blank()),
     
     DAI_numeric = site_corrected_analysis_suitable_data %>%
       rowid_to_column(var = 'ID') %>%
@@ -53,7 +54,8 @@ descriptorScatterPlots <- function(site_corrected_analysis_suitable_data,decline
            x = 'Descriptor value') +
       theme(plot.title = element_text(face = 'bold'),
             axis.title = element_text(face = 'bold'),
-            strip.text = element_markdown()),
+            strip.text = element_markdown(),
+            panel.grid = element_blank()),
     
     DAI_factor = site_corrected_analysis_suitable_data %>%
       select_if(is.factor) %>%
@@ -61,8 +63,7 @@ descriptorScatterPlots <- function(site_corrected_analysis_suitable_data,decline
       bind_cols(select(decline_indexes,DAI)) %>%
       gather(Descriptor,Value,-ID,-DAI) %>%
       mutate(Descriptor = factor(Descriptor) %>%
-               fct_recode(`Agrilus exit hole density (m<sup>-2</sup>)` = 'Agrilus exit hole density (m^-2)',
-                          `Crown volume (m<sup>3</sup>)` = 'Crown volume (m^3)')) %>%
+               fct_recode(`Small circular shaped<br>exit holes` = "Small circular shaped exit holes")) %>%
       ggplot(aes(x = Value,y = DAI)) +
       geom_point(shape = 21,fill = ptol_pal()(1)) +
       theme_bw() +
@@ -71,6 +72,7 @@ descriptorScatterPlots <- function(site_corrected_analysis_suitable_data,decline
            x = 'Descriptor value') +
       theme(plot.title = element_text(face = 'bold'),
             axis.title = element_text(face = 'bold'),
-            strip.text = element_markdown())
+            strip.text = element_markdown(),
+            panel.grid = element_blank())
   )
 }
